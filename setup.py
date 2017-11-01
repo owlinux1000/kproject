0d8faca169f9709e2c95022ddbdcfca48501eeff
encry1024 <pgm3rdlinuxor1000@gmail.com>
2017-11-01 16:12:47 +0900

    fix

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author=author,
     author_email=author_email,
     license=license,
-    packages=find_packages(),
+    packages=['kproject']
     entry_points="""
     [console_scripts]
     kproject = kproject.kproject:main

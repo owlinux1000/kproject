eae479178d1e05222e7fb39809355427333c3687
encry1024 <pgm3rdlinuxor1000@gmail.com>
2017-11-01 15:49:23 +0900

    fix main.py

--- bin/main.py
+++ bin/main.py
@@ -62,7 +62,7 @@ def _run_cmd(args):
         fout.write('\n'.join(hist) + '\n')
 '''        
 
-if __name__ == '__main__':
+def main():
 
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers()
@@ -85,3 +85,7 @@ if __name__ == '__main__':
 
     sys.exit(1)
 
+
+if __name__ == '__main__':
+    
+    main()
